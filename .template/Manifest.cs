3f965770d4e5f5efc5c00a8e8ad59910328a0ae2
KingRainbow44 <kobedo11@gmail.com>
2024-08-03 17:06:05 -0400

    misc: Add plugin template

---
+++ .template/Manifest.cs
@@ -0,0 +1,8 @@
+﻿using FreakyProxy;
+
+[assembly: PluginManifest(ManifestVersion = 1,
+    Name = "Template",
+    Author = "<your name>",
+    Description = "A template for creating GatoProxy plugins.",
+    MainClass = "Template.Plugin",
+    Version = "1.0")]
---
+++ .template/Plugin.cs
@@ -0,0 +1,9 @@
+﻿using FreakyProxy;
+
+namespace Template;
+
+public class Template(PluginInfo info) : FreakyProxy.Plugin(info) {
+    public override void OnLoad() {
+        Logger.Info("Template plugin loaded.");
+    }
+}
