1ea934d8ebdb21722e0c0cd979bd36b948206aff
KingRainbow44 <kobedo11@gmail.com>
2024-08-03 18:04:22 -0400

    fix(template): Set default class name

--- .template/Plugin.cs
+++ .template/Plugin.cs
@@ -2,7 +2,7 @@
 
 namespace Template;
 
-public class Template(PluginInfo info) : FreakyProxy.Plugin(info) {
+public class Plugin(PluginInfo info) : FreakyProxy.Plugin(info) {
     public override void OnLoad() {
         Logger.Info("Template plugin loaded.");
     }
