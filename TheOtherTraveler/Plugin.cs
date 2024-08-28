bed66777006febd4c349cc8a48e9a44d7fddeedf
KingRainbow44 <kobedo11@gmail.com>
2024-08-28 00:20:18 -0400

    fix(TheOtherTraveler): Add support for new 5.0 removal of `AvatarInfo`
    good job mihoyo!

--- TheOtherTraveler/Plugin.cs
+++ TheOtherTraveler/Plugin.cs
@@ -84,6 +84,56 @@ public class Plugin(PluginInfo info) : FreakyProxy.Plugin(info) {
         });
     }
 
+    /// <summary>
+    /// Translates all avatar info to the new avatar.
+    /// </summary>
+    private static void UpdateSceneAvatarInfo(SceneAvatarInfo info) {
+        var oldAvatarId = info.AvatarId;
+        if (!Travelers.Contains(oldAvatarId)) return;
+
+        info.ExcelInfo = null;
+
+        // Determine the new avatar ID.
+        var newAvatarId = info.AvatarId = oldAvatarId switch {
+            TravelerMale => TravelerFemale,
+            TravelerFemale => TravelerMale,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+        // Get current skill depot index.
+        var oldAvatarConfig = GameData.AvatarData[oldAvatarId];
+
+        var oldDepotId = info.SkillDepotId;
+        var depotIndex = oldAvatarConfig.CandSkillDepotIds.IndexOf(oldDepotId);
+
+        // Now that we have the index for the old avatar, we need to find the new one.
+        // We do this by getting the candidate skill depot IDs for the new avatar and
+        // selecting the one at the same index as the old one.
+        var newDepotId = GameData.AvatarData[newAvatarId].CandSkillDepotIds[depotIndex];
+        var skillDepot = GameData.AvatarSkillDepotData[newDepotId];
+
+        // Update all skills.
+
+        skillDepot.AllSkills().ForEach(id =>
+            info.SkillLevelMap[id] = 1);
+
+        info.InherentProudSkillList.Clear();
+        skillDepot.ConstellationSkills
+            .Where(data => data.ProudSkillGroupId > 0)
+            .Select(data => data.ProudSkillGroupId * 100 + 1)
+            .ToList()
+            .ForEach(id => info.InherentProudSkillList.Add(id));
+
+        info.TalentIdList.Clear();
+        info.TalentIdList.AddRange(skillDepot.Talents);
+
+        info.SkillLevelMap.Clear();
+        info.ProudSkillExtraLevelMap.Clear();
+        skillDepot.AllSkills().ForEach(skill => {
+            info.SkillLevelMap[skill] = 11;
+        });
+    }
+
     [Handler(CmdID.SceneEntityAppearNotify)]
     public static ValueTask<PacketResult> HandleSceneEntityAppearNotify(Session session, PacketHead _,
         SceneEntityAppearNotify msg) {
@@ -93,50 +143,7 @@ public class Plugin(PluginInfo info) : FreakyProxy.Plugin(info) {
             var info = entity.Avatar;
             if (info is null) continue;
 
-            var oldAvatarId = info.AvatarId;
-            if (!Travelers.Contains(oldAvatarId)) continue;
-
-            info.ExcelInfo = null;
-
-            // Determine the new avatar ID.
-            var newAvatarId = info.AvatarId = oldAvatarId switch {
-                TravelerMale => TravelerFemale,
-                TravelerFemale => TravelerMale,
-                _ => throw new ArgumentOutOfRangeException()
-            };
-
-            // Get current skill depot index.
-            var oldAvatarConfig = GameData.AvatarData[oldAvatarId];
-
-            var oldDepotId = info.SkillDepotId;
-            var depotIndex = oldAvatarConfig.CandSkillDepotIds.IndexOf(oldDepotId);
-
-            // Now that we have the index for the old avatar, we need to find the new one.
-            // We do this by getting the candidate skill depot IDs for the new avatar and
-            // selecting the one at the same index as the old one.
-            var newDepotId = GameData.AvatarData[newAvatarId].CandSkillDepotIds[depotIndex];
-            var skillDepot = GameData.AvatarSkillDepotData[newDepotId];
-
-            // Update all skills.
-
-            skillDepot.AllSkills().ForEach(id =>
-                info.SkillLevelMap[id] = 1);
-
-            info.InherentProudSkillList.Clear();
-            skillDepot.ConstellationSkills
-                .Where(data => data.ProudSkillGroupId > 0)
-                .Select(data => data.ProudSkillGroupId * 100 + 1)
-                .ToList()
-                .ForEach(id => info.InherentProudSkillList.Add(id));
-
-            info.TalentIdList.Clear();
-            info.TalentIdList.AddRange(skillDepot.Talents);
-
-            info.SkillLevelMap.Clear();
-            info.ProudSkillExtraLevelMap.Clear();
-            skillDepot.AllSkills().ForEach(skill => {
-                info.SkillLevelMap[skill] = 11;
-            });
+            UpdateSceneAvatarInfo(info);
         }
 
         return Intercept;
@@ -146,10 +153,10 @@ public class Plugin(PluginInfo info) : FreakyProxy.Plugin(info) {
     public static ValueTask<PacketResult> HandleSceneTeamUpdateNotify(Session session, PacketHead _,
         SceneTeamUpdateNotify msg) {
         foreach (var avatar in msg.TeamInfo) {
-            var avatarInfo = avatar.AvatarInfo;
+            var avatarInfo = avatar?.SceneEntityInfo?.Avatar;
             if (avatarInfo is null) continue;
 
-            UpdateAvatarInfo(avatarInfo);
+            UpdateSceneAvatarInfo(avatarInfo);
         }
         return Intercept;
     }
