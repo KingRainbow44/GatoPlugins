05ba7b983410dfc76f1a8ba1e3e4a213d67f5af6
KingRainbow44 <kobedo11@gmail.com>
2024-09-09 14:00:00 +0100

    feat(DeathSwap): Finish core game mechanic

--- DeathSwap/Plugin.cs
+++ DeathSwap/Plugin.cs
@@ -2,26 +2,62 @@
 using Common.Util;
 using FreakyProxy;
 using FreakyProxy.Events;
+using Timer = System.Timers.Timer;
 
 namespace DeathSwap;
 
 public class Plugin(PluginInfo info) : FreakyProxy.Plugin(info) {
-    public static bool Override = false;
+    public static bool Running;
+
+    public static DeathSwapPlayer? Source, Target;
 
     public static readonly Dictionary<string, string> Accounts = new();
     public static readonly Dictionary<string, GetPlayerTokenReq> Tokens = new();
     public static readonly Dictionary<string, PlayerLoginReq> Logins = new();
 
+    private static Config _config;
+
     public override void OnLoad() {
+        _config = this.GetConfig(new Config());
         CommandProcessor.RegisterAllCommands("DeathSwap");
-
-        #region Register Events
-
         PluginManager.AddEventListener<PlayerCreationEvent>(OnPlayerCreationEvent);
 
-        #endregion
-
         Logger.Info("DeathSwap plugin loaded.");
+    }
+
+    public override void OnEnable() {
+        RunTask();
+    }
+
+    public override void OnUnload() {
+        Running = false;
+    }
+
+    /// <summary>
+    /// Starts a repeated task for the death swap game.
+    /// </summary>
+    public static void RunTask() {
+        Task.Run(async () => {
+            // Wait a random duration of time.
+            var span = TimeSpan.FromMilliseconds(
+                Utils.Random(_config.MinInterval, _config.MaxInterval));
+            await Task.Delay(span);
+
+            if (Running &&
+                Source is not null &&
+                Target is not null) {
+                // Run the swap function.
+                await Source.SwapWith(Target);
+
+                // Clear the source and target.
+                // These should be re-populated by the next interval.
+                Source = null;
+                Target = null;
+            }
+
+            // Re-run the loop.
+            RunTask();
+        });
     }
 
     /// <summary>
