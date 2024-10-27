90e006f9a73bdf1037cfba5cca8402c75998900b
KingRainbow44 <kobedo11@gmail.com>
2024-10-27 04:35:58 -0400

    misc: Modify `copy-all.ts` to include another file of the user's choosing

--- copy-all.ts
+++ copy-all.ts
@@ -3,6 +3,8 @@ import { readdir } from "node:fs/promises";
 
 const otherDir = process.env["FINAL_COPY"] || undefined;
 
+const otherFile = process.env["OTHER_FILE"] || undefined;
+
 const plugins = new Glob("*");
 for await (const plugin of plugins.scan({ onlyFiles: false })) {
     try {
@@ -22,3 +24,21 @@ for await (const plugin of plugins.scan({ onlyFiles: false })) {
 
     }
 }
+
+await (async () => {
+    if (otherFile) {
+        const artifact = Bun.file(otherFile);
+        if (!await artifact.exists()) return;
+
+        const split = otherFile.split("/");
+        const fileName = split[split.length - 1];
+
+        // Copy to `dist/artifact` directory.
+        await Bun.write(`dist/${fileName}`, artifact);
+
+        if (otherDir) {
+            // Copy to `dist/otherDir/artifact` directory.
+            await Bun.write(`${otherDir}/${fileName}`, artifact);
+        }
+    }
+})();
