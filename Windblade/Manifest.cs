bc0a3a3b74bb528854ca50c61b26672dda4360f0
KingRainbow44 <kobedo11@gmail.com>
2025-07-23 12:36:10 -0400

    fix(Windblade): bump manifest version to `3`

--- Windblade/Manifest.cs
+++ Windblade/Manifest.cs
@@ -1,6 +1,6 @@
 ﻿using FreakyProxy;
 
-[assembly: PluginManifest(ManifestVersion = 2,
+[assembly: PluginManifest(ManifestVersion = 3,
     Name = "Windblade",
     Author = "KingRainbow44",
     Description = "The ultimate WindSeedClientNotify utility.",
